0dd104db2898ac3643c632eacf2446705afe95e7
KowshikSuggala25 <saikowshiksuggala9390@gmail.com>
2025-08-26 22:46:24 +0530

    Chunk increased

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  build: {
+    chunkSizeWarningLimit: 1600, 
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
